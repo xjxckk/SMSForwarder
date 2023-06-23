ca6186d0aaf662a98366ba83d7472db5b22893eb
xJxckk <>
2023-06-23 15:11:44 +0100

    Updates

--- SMSForwarder.py
+++ SMSForwarder.py
@@ -1,29 +1,50 @@
 from textwrap import dedent
-from flask import Flask, request
+from flask import Flask, request, redirect
 from error_alerts import telegram
 
-logger = logger()
-log, current_time = logger.log, logger.current_time
+alerts = telegram(token='1736901269:AAFD99l-rVHfmPY70huJECgCNnZFCFq5c00') # @jxdevbot
 
-alerts = telegram(token='1736901269:AAFD99l-rVHfmPY70huJECgCNnZFCFq5c00', channel=-920168991, logger=logger) # @jxdevbot / Blackjack channel
+syndicate_channel = -920168991 # 'Blackjack' channel
+'''
+Syndicate numbers (https://dev.jxck.ml/sms-forwarder/syndicate/):
++39 339 990 8320
+'''
+tristzan_channel = -965988103 # 'SMS forwarding' channel
+'''
+Tristzan numbers (https://dev.jxck.ml/sms-forwarder/tristzan/):
++39 339 995 8629
+'''
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def receive_sms_and_forward():
-    '''Receive incoming text messages and forward to Telegram'''
-    from_number = request.form['From']
-    to_number = request.form['To']
-    body = request.form['Body']
-    print('Received SMS')
-    print()
-    message = dedent(f'''\
-        From: {from_number}
-        To: {to_number}
-        Message: {body}
-        ''')
-    alerts.send_message(message)
-    return '<Response></Response>'
+@app.route('/')
+def redirect_to_main_site():
+    return redirect('https://jxck.ml')
+
+@app.route('/<client>/', methods=['GET', 'POST'])
+def receive_sms_and_forward(client):
+    if request.method == 'POST':
+        '''Receive incoming text messages and forward to Telegram'''
+        from_number = request.form['From']
+        to_number = request.form['To']
+        body = request.form['Body']
+        print('Received SMS')
+        print()
+
+        message = dedent(f'''\
+            From: {from_number}
+            To: {to_number}
+            Message: {body}
+            ''')
+
+        if client == 'tristzan':
+            alerts.send_message(message, channel=tristzan_channel)
+        elif client == 'syndicate':
+            alerts.send_message(message, channel=syndicate_channel)
+        return '<Response></Response>'
+
+    else:
+        return redirect_to_main_site()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- test_server.py
+++ test_server.py
@@ -2,4 +2,4 @@ import requests
 
 # requests.get('http://localhost:5000/sms')
 # requests.get('http://192.168.1.2:5000/sms')
-requests.get('http://86.3.247.9:5000/sms')
+requests.post('http://86.3.247.9:5000/sms')
